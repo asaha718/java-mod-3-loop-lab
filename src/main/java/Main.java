11066726f873d7b901cc8e741939f69aa026d151
Anug Saha <anugsaha@gmail.com>
2022-06-22 13:56:10 -0400

    working while loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
- 
+
 public class Main {
     public static void main(String[] args) {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
         int currentYear = startingYear;
-        while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
+        while (currentYear < targetYear ) {
+            if(currentYear - startingYear == 1 ) { 
+                System.out.println((currentYear - startingYear) + " year has passed");
+            }
+            
+            if ( currentYear - startingYear > 1) { 
+                System.out.println((currentYear - startingYear) + " years have passed");  
+            } 
+            
             // conditional logic based on the current year
             currentYear++;
         }
